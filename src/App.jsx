42b3afc7995677a4c18cf3f1db5062e526c4cd39
BagusFary <bagusfaryananta@gmail.com>
2023-10-27 09:51:09 +0700

    Todolist

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Form/Login";
-import CreateAccount from "./Form/CreateAccount";
-import Formulir from "./Form/Formulir";
-import FormulirPerpus from "./Form/FormulirPerpus";
+import Login from "./Pages/Login";
+import CreateAccount from "./Pages/CreateAccount";
+import Formulir from "./Pages/Formulir";
+import FormulirPerpus from "./Pages/FormulirPerpus";
+import Todolist from "./Pages/Todolist";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/register" element={<CreateAccount/>}/>
         <Route path="/formulir" element={<Formulir/>}/>
         <Route path="/formulirperpus" element={<FormulirPerpus/>}/>
+        <Route path="/todolist" element={<Todolist/>}/>
       </Routes>
     </Router>
   );
